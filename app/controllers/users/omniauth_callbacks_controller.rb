f10f731a92401f3fdb4ca17c008a73c0a246daa0
bachikori <ya.7117.goo@gmail.com>
2023-04-10 21:35:05 +0900

    コメントアウト削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -23,14 +23,12 @@ module Users
     # end
 
     def github
-      # request.env["omniauth.auth"]にGitHubから送られてきたデータが入っている
-
       @user = User.find_for_github_oauth(request.env['omniauth.auth'])
 
       if @user.persisted? # データベースに保存されていればログイン成功
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-      else # ログイン失敗
+      else
         session['devise.github_data'] = request.env['omniauth.auth']
         redirect_to new_user_registration_url
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,18 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers: [:github]
 
   validates :nickname, presence: true
-  # validates :birthday
-  # validates :tel_number
   validates :email, presence: true, uniqueness: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
   def self.find_for_github_oauth(auth)
-    # binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-      # 名前を取得するときはこのように書く（今回はUserモデルにname属性がないのでエラーなる）
       user.nickname = auth.info.nickname
       user.email = auth.info.email
-      # 任意の20文字の文字列を作成する
+
       user.password = Devise.friendly_token[0, 20]
     end
   end
 
-  # 最後に使います。
   def self.create_unique_string
     SecureRandom.uuid
   end
