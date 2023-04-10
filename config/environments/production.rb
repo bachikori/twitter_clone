2599f26df84361a9037eddfac8a5b4750dd8e012
bachikori <ya.7117.goo@gmail.com>
2023-04-10 20:40:26 +0900

    メイラーの設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: Rails.application.credentials.gmail[:user_name],
+    user_name: Rails.application.credentials.gmail[:username],
     password: Rails.application.credentials.gmail[:password],
     authentication: :plain,
     enable_starttls_auto: true
