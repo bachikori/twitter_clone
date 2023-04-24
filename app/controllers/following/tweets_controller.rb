5f9e92d5f723a46f8e965c0cbcd1887a232c778d
bachikori <ya.7117.goo@gmail.com>
2023-04-24 22:06:03 +0900

    フォロー中の人のみツイート表示

--- app/controllers/following/tweets_controller.rb
+++ app/controllers/following/tweets_controller.rb
@@ -3,14 +3,8 @@
 module Following
   class TweetsController < ApplicationController
     def index
-      @tweet_array = []
-      current_user.following_user.each do |user|
-        following_user_tweets = user.tweets.order(created_at: :desc)
-        @tweet_array.concat(following_user_tweets)
-      end
-      current_user_tweets = current_user.tweets.order(created_at: :desc)
-      @tweet_array.concat(current_user_tweets)
-      @tweets = Kaminari.paginate_array(@tweet_array.sort { |a, b| b.created_at.to_f <=> a.created_at.to_f }).page(params[:page])
+      follow_user = current_user.following_user
+      @tweets = Tweet.where(user: follow_user).order(created_at: 'desc').page(params[:page])
     end
 
     def create
